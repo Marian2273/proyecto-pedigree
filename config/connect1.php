7c1cb8d9da609e968e7d2a5f43a6fcc4ed70eb44
Mariana <marianabelgrano@gmail.com>
2022-09-08 22:14:46 -0300

    BIS kennel

---
+++ config/connect1.php
@@ -0,0 +1,11 @@
+<?php
+$config = parse_ini_file('../privado-pedigree/config.ini'); 
+// Try and connect to the database
+$mysqli = mysqli_connect($config['servername'],$config['username'],$config['password'],$config['dbname']);
+mysqli_query($mysqli , "SET SESSION sql_mode = ''");
+$mysqli->set_charset("utf8");
+// If connection was not successful, handle the error
+if($mysqli === false) {
+            // Handle error - notify administrator, log to a file, show an error screen, etc.
+        }
+?>
